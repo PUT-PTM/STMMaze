9dae8cb5927641308fab8a649103408fe344c6be
Rodnok <konrad.kahl@wp.pl>
2015-05-17 19:14:31 +0200

    Teleport character over reaching an edge on vertical axis

--- Assets/Skrypty/charMove.js
+++ Assets/Skrypty/charMove.js
@@ -11,7 +11,12 @@
  var moveLeft : KeyCode;
  
  var posx = 0;
- var posy = 0;
+ var posy = -1;
+ 
+ var timer = 0.0;
+ 
+ var player : GameObject;
+ var spawnPos : Vector2;
 
  
  function Start() {
@@ -20,7 +25,18 @@
  }
  
  function Update() {
- 
+ 		
+ 		timer += Time.deltaTime;
+ 	if(posy == -1)
+     {
+        pos += Vector2.up;
+        posy = 0;
+     }
+ if(posy == 3)
+     {
+        pos -= Vector2.up;
+        posy = 2;
+     }
      if (Input.GetKey(moveRight) && tr.position == pos) {
      	if(posx <9)
      	{
@@ -36,11 +52,31 @@
         }
      }
      else if (Input.GetKeyDown(moveUp) && tr.position == pos) {
+    
          pos += Vector2.up;
+         posy++;
+          Debug.Log(posy);
      }
      else if (Input.GetKeyDown(moveDown) && tr.position == pos) {
+      		
          pos -= Vector2.up;
+         posy--;
+          Debug.Log(posy); 
      }
      
      transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+     
+     if(posy == 3 && Input.GetKeyDown(moveUp)){
+         posy= -1;
+        spawnPos = Vector2(pos.x,-2.003905);
+     	var temp_spawn_player = Instantiate(gameObject, spawnPos, Quaternion.identity);
+     	Destroy(gameObject,0.1);
+     }
+     if(posy == -1 && Input.GetKeyDown(moveDown)){
+        posy= 3;
+        spawnPos = Vector2(pos.x,1.996095);
+     	var temp_spawn_player2 = Instantiate(gameObject, spawnPos, Quaternion.identity);
+     	Destroy(gameObject,0.1);
+     }
+     
  }   
