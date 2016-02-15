5eba22316d6a3c4e020aef122e447afe36f776d1
Rodnok <konrad.kahl@wp.pl>
2016-02-15 23:11:50 +0100

    updated STM32 accelerometer file and tested (works perfectly)

--- STM32/accelerometer.js
+++ STM32/accelerometer.js
@@ -1,4 +1,5 @@
 function onInit() {
+  SPI1.setup({sck:A5, miso:A6, mosi:A7});
   SPI1.send([0x20,0b01000111], E3);
 }
 
@@ -10,9 +11,29 @@ function getAcc() {
   if (accy>127) accy-=256;
   avrx = 0.1*accx + 0.9*avrx;
   avry = 0.1*accy + 0.9*avry;
-  digitalWrite(LED2, avrx > 64);
-  digitalWrite(LED4, avrx < -64);
-  digitalWrite(LED1, avry > 64);
-  digitalWrite(LED3, avry < -64);
+  if( avrx > 20) {
+    digitalWrite(LED2, 1);
+  } else {
+    digitalWrite(LED2, 0);
+  }
+  if( avrx < -20) {
+    digitalWrite(LED4, 1);
+  } else {
+    digitalWrite(LED4, 0);
+  }
+  if( avry > 20) {
+    digitalWrite(LED1, 1);
+  }
+  else {
+    digitalWrite(LED1, 0);
+  }
+  if( avry < -20) {
+    digitalWrite(LED3, 1);
+  } else {
+    digitalWrite(LED3, 0);
+  }
+
 }
-onInit();setInterval(getAcc, 10);
+onInit();
+
+setInterval(getAcc, 10);
